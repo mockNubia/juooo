6dadfe17677f5dec4a5f32f7c6b40b581552b46d
moguo960922 <635475486@qq.com>
2018-12-05 17:53:49 +0800

    样式

--- src/pages/Show/SHeader.js
+++ src/pages/Show/SHeader.js
@@ -1,5 +1,7 @@
 import React,{ Component } from 'react';
+import {Link} from 'react-router-dom'
 import './Show.scss'
+import SConnent from './SConnent.js'
 
 class SHeader extends Component {
     constructor(props) {
@@ -9,54 +11,73 @@ class SHeader extends Component {
 			nav: 0,
 			navs: [{
 				title: "全部",
-				href: "/show/all/",
+				href: "/show?cid=-1",
 				category: 0
 			}, {
 				title: "演唱会",
-				href: "/show/vocalconcert/",
+				href: "/show?cid=-1&caid=35",
 				category: 35
 			}, {
 				title: "音乐会",
-				href: "/show/musicale/",
+				href: "/show?cid=-1&caid=36",
 				category: 36
 			}, {
 				title: "话剧歌剧",
-				href: "/show/drama/",
+				href: "/show?cid=-1&caid=37",
 				category: 37
             }, {
 				title: "儿童亲子",
-				href: "/show/children/",
+				href: "/show?cid=-1&caid=38",
 				category: 38
             }, {
 				title: "音乐剧",
-				href: "/show/melodrama/",
+				href: "/show?cid=-1&caid=79",
 				category: 79
             }, {
 				title: "舞蹈芭蕾",
-				href: "/show/ballet/",
+				href: "/show?cid=-1&caid=86",
 				category: 86
             }, {
 				title: "戏曲综艺",
-				href: "/show/variety/",
+				href: "/show?cid=-1&caid=91",
 				category: 91
             }, {
 				title: "展览",
-				href: "/show/exhibition/",
+				href: "/show?cid=-1&caid=99",
 				category: 99
             }]
         };
     };
-    navigateTo(index, e) {
-		this.setState({
-			nav: index
+	componentWillMount(){
+		var param = (this.props.history.location.search).split('=');
+		if(!param[2]){
+			this.setState({nav:0});
+		}else{
+			this.setState({nav:Number(param[2])});
+		}
+		this.$post({
+			url:'/jc/Show/getShowList',
+			data:{
+				city_id: -1,
+                category: Number(param[2]),
+                keywords:'',
+                activity_id: 0,
+                sort_type: 0,
+                page: 1
+			}
 		})
-		this.props.history.push(this.state.navs[index].href)
-	};
+	}
+	componentDidMount(){
+		
+	}
+	reload(){
+		window.location.reload();
+	}
 	render(){
 		return(
 			<div className="show_header">
                 <div className="show_header_top">
-                    <div className="fl">
+                    <div className="fl city">
                         广州<i className="fa fa-angle-down" aria-hidden="true"></i>
                     </div>
                     <div className="fl search">
@@ -68,27 +89,30 @@ class SHeader extends Component {
                     </div>
                 </div>
                 <div className="show_header_bottom">
+				
                     <ul>
                     {
 						(() => {
 							return this.state.navs.map((item, index) => {
 				    			return (
-									<li onClick={
-										this.navigateTo.bind(this, index)
-										
-									} key={index} className={
-										index === this.state.nav ? "active" : ""
+									<Link 
+										to = {item.href}
+										onClick = {this.reload}
+										key={index} className={
+										item.category === this.state.nav ? "active" : ""
 									}>
 										<span>
-											{item.title}<em>{item.category}</em>
+											{item.title}
 										</span>
-									</li>
+									</Link>
 							    	)
 						    	})
 						    })()
 						}
                     </ul>
                 </div>
+				<div className="splic"></div>
+				<SConnent props = {this.state.nav}/>
 			</div>
 		)
 	}
